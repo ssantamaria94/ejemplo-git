639dc7858f659975c48896cc5c9c306594e41419
ssantamaria94 <s.santamaria.most@gmail.com>
2026-04-22 17:32:43 +0200

    Added README

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hola 2º de DAM :)");
+        System.out.println("¡Bienvenidos al sistema de control de versiones con Git!");
         System.out.println("Adios 2º de DAM :)");
     }
 }
